c3ea62dfd5dcd6f9c03bf071985957245ef556b1
Guaaaaaaaan <2437472663a@gmail.com>
2025-10-31 12:33:44 +0800

    InitialCommit

---
+++ SADFinalProjectMerrick/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SADFinalProjectMerrick.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
